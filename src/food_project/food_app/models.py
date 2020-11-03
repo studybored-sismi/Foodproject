e52ba2097d5175af528a2809a5e5e27c558c392a
ashu <ashu@gmail.com>
2020-11-03 11:30:24 +0530

    added model and apply migrations

--- src/food_project/food_app/models.py
+++ src/food_project/food_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
